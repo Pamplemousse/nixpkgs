3b987f8674befc5ce5d764b9c391de95817dbd6a
Pamplemousse <xav.maso@gmail.com>
2019-12-20 11:22:40 -0700

    pythonPackages: 4.41.0 -> 4.51.1

--- pkgs/development/python-modules/hypothesis/default.nix
+++ pkgs/development/python-modules/hypothesis/default.nix
@@ -9,7 +9,7 @@ buildPythonPackage rec {
   # pytz fake_factory django numpy pytest
   # If you need these, you can just add them to your environment.
 
-  version = "4.41.0";
+  version = "4.51.1";
   pname = "hypothesis";
 
   # Use github tarballs that includes tests
@@ -27,14 +27,10 @@ buildPythonPackage rec {
   checkInputs = [ pytest pytest_xdist flaky mock pexpect ];
   inherit doCheck;
 
-  checkPhase = ''
-    rm tox.ini # This file changes how py.test runs and breaks it
-    py.test tests/cover
-  '';
-
   meta = with lib; {
     description = "A Python library for property based testing";
     homepage = https://github.com/HypothesisWorks/hypothesis;
     license = licenses.mpl20;
+    maintainers = with maintainers; [ pamplemousse ];
   };
 }
